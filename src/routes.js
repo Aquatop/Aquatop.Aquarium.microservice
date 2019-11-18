2ae2200db100aa48deade531f3a819c4a96ad5fe
LucasLermen <lucas.arthur.lermen@gmail.com>
2019-11-18 20:14:11 -0300

    Add validator.
    Co-authored-by: lucassiqz <lucassiqzro@gmail.com>
    Co-authored-by: lukassxp <lpalucas.10@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import AquariumController from './app/controllers/AquariumController';
 import PinController from './app/controllers/PinController';
-
+import validateAquariumStore from './app/validators/AquariumStore'
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -11,8 +11,7 @@ routes.get('/', (req, res) => res.json({ ok: true }));
 
 routes.use(authMiddleware);
 
-// Adicionar validator
-routes.put('/aquarium/:name', AquariumController.update);
+routes.put('/aquarium/:name',  validateAquariumStore, AquariumController.update);
 routes.get('/aquarium/:name', AquariumController.index);
 routes.get('/aquarium', AquariumController.list);
 
